8c8e7b613b6f2e0e1df1778c006a6bf5fec13d21
Fomber <37545484+Fomber@users.noreply.github.com>
2019-01-02 01:22:37 +0800

    今晚学习了用xpath进行网页内容抓取。效果不错

--- book_spider/spiders/book.py
+++ book_spider/spiders/book.py
@@ -9,4 +9,4 @@ class book_Spider(scrapy.Spider):
         names = response.xpath('//li/article[@class="product_pod"]/h3/a')
         for book_name in names:
             print(book_name.attrib.get('title'))
-            #why my vcs is lost?54466666666666666666666666233333333333333333332222222051500
+            #w1500hy my vcs is lost?5446666666666666666666666623333333333333333333222222205
